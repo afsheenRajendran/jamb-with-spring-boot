a5108fab29cdba7ecf2d7b66b36965e96e074745
Afsheen Rajendran <afsheen.rajendran@gmail.com>
2019-05-10 16:50:37 -0500

    Added dependency on thymeleaf. Able to see webpage at http://localhost:8081

---
+++ src/main/java/com/ithellam/boot/controllers/ProductController.java
@@ -0,0 +1,19 @@
+package com.ithellam.boot.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.ui.Model;
+
+@Controller
+public class ProductController {
+    @Value("${spring.application.name}")
+    String appName;
+
+    @GetMapping("/")
+    public String homePage(Model model) {
+        model.addAttribute("appName", appName);
+        return "home";
+    }
+}
